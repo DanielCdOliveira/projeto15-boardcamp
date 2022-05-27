7bbe14d7f3ffcc8763ecec2d55ca6efefb9748e5
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-05-27 10:28:45 -0300

    POST /games

--- src/app.js
+++ src/app.js
@@ -10,28 +10,47 @@ app.use(cors());
 app.use(json());
 dotenv.config();
 // CATEGORIES
-app.get("/categories",(req, res) => {
-    connection.query('SELECT * FROM categories').then(categories=>{res.send(categories.rows)});
+app.get("/categories", (req, res) => {
+  connection.query("SELECT * FROM categories").then((categories) => {
+    res.send(categories.rows);
+  });
 });
 app.post("/categories", (req, res) => {
-    const {name} = req.body
-    connection.query('INSERT INTO categories (name) VALUES ($1)',[name]).then(()=>{res.sendStatus(201)});
+  const { name } = req.body;
+  connection
+    .query("INSERT INTO categories (name) VALUES ($1)", [name])
+    .then(() => {
+      res.sendStatus(201);
+    });
 });
 // GAMES
 app.get("/games", (req, res) => {
-  connection.query('SELECT  games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id').then(games=>{res.send(games.rows)});
+  connection
+    .query(
+      'SELECT  games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id'
+    )
+    .then((games) => {
+      res.send(games.rows);
+    });
 });
 app.post("/games", (req, res) => {
-
-
+  const game = req.body;
+  console.log(game);
+   connection.query(`
+   INSERT INTO games ("name","image","stockTotal","categoryId","pricePerDay")
+   VALUES ($1,$2,$3,$4,$5)`,
+      [
+        game.name,
+        game.image,
+        game.stockTotal,
+        game.categoryId,
+        game.pricePerDay,
+      ]
+    )
+    .then(() => {
+      res.sendStatus(201);
+    });
 });
-
-
-
-
-
-
-
 
 const port = process.env.PORT;
 app.listen(port, () =>
