679c4d4debb0426c9f31ac26c214ae279ded0412
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-05-27 10:12:37 -0300

    GET /games

--- src/app.js
+++ src/app.js
@@ -9,17 +9,29 @@ const app = express();
 app.use(cors());
 app.use(json());
 dotenv.config();
-
+// CATEGORIES
 app.get("/categories",(req, res) => {
     connection.query('SELECT * FROM categories').then(categories=>{res.send(categories.rows)});
 });
-
 app.post("/categories", (req, res) => {
     const {name} = req.body
     connection.query('INSERT INTO categories (name) VALUES ($1)',[name]).then(()=>{res.sendStatus(201)});
 });
-app.get("/rentals", (req, res) => {});
-app.get("/rentals", (req, res) => {});
+// GAMES
+app.get("/games", (req, res) => {
+  connection.query('SELECT  games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id').then(games=>{res.send(games.rows)});
+});
+app.post("/games", (req, res) => {
+
+
+});
+
+
+
+
+
+
+
 
 const port = process.env.PORT;
 app.listen(port, () =>
